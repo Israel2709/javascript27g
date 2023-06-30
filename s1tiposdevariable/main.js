e00457001bc336ca5119ff821a17f383d3f6f12f
Israel Salinas <israel.salinas.m@gmail.com>
2023-06-29 22:19:03 -0600

    s1

---
+++ s1tiposdevariable/main.js
@@ -0,0 +1,42 @@
+// const sample = "Hola koders"; /*<-Declaración o definicion*/
+
+// sample = 27; /*<-Reasignación*/
+
+// const sample = true; /*<-Redeclaración */
+
+// console.log(sample);
+
+// let bar = {};
+
+// bar /*{}*/.name /*{name: undefined}*/ = "x"; /*{name:"x"}*/
+
+// console.log(bar); /*{name:"x"}*/
+// console.log(bar.name); /*"x"*/
+
+// let koders = [
+//     "Ara",
+//     "Vic",
+//     "Jesus"
+// ]
+
+// const koders25 = [
+//     {
+//         name:"lau"
+//     },
+//     7
+// ]
+
+let a = 50;
+let b = 3;
+
+/*Operadores aritméticos*/
+// + => adicion
+let adicion = a + b; /*53*/
+// - => sustracción
+let sustraccion = a - b; /*47*/
+// * => multiplicación
+let multiplicacion = a * b; /*150*/
+// / => división
+let division = a / b; /*16.67*/
+// % =>  módulo
+let modulo = a % b; /*2*/
